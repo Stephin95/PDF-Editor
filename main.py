ca0a3a845f4e2b75c60a6484d7a7c2efefe39019
Stephin Thomas <stephinthomas95@gmail.com>
2021-11-21 22:56:39 +0000

    added loading and saving dialogs

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ contains a root Widget.
 '''
 
 import kivy
-
+import os
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.properties import ObjectProperty
+# from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
 
 
 class Screen_manager(ScreenManager):
@@ -23,8 +28,47 @@ class Screen_manager(ScreenManager):
 class Main_window(Screen):
     pass
 class Secondary_window(Screen):
-    pass
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
 
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        file_path=os.path.join(path, filename[0])
+        print(file_path)
+        # with open(os.path.join(path, filename[0])) as stream:
+        #     self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text) #considering output file as text for now.
+
+        self.dismiss_popup()
+    pass
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 class PDFApp(App):
     def build(self):
