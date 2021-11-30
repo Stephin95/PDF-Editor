a0a0cc1a7b27b0f29ce9d477e209875dbd4b648d
Stephin Thomas <stephinthomas95@gmail.com>
2021-11-30 10:59:40 +0000

    Changed how the save path is obtained for combatibility with other platforms

--- main.py
+++ main.py
@@ -2,252 +2,247 @@
 import os
 from kivy.app import App
 from pathlib import Path
+
 # from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty,StringProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from pdf_E import PdfEditor
 from kivy.clock import Clock
 from kivy.utils import platform
-if platform == "android":
-	from android.permissions import request_permissions, Permission
 
-	request_permissions(
-		[Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE]
-	)
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+
+    request_permissions(
+        [Permission.WRITE_EXTERNAL_STORAGE, Permission.READ_EXTERNAL_STORAGE]
+    )
 
 
 # Builder.load_file("C:\\Users\\steph\\Home\\Python\\Pdf_editor\\pdf.kv")
 class Screen_manager(ScreenManager):
-	pass
+    pass
 
 
 class Main_window(Screen):
-	pass
+    pass
 
 
 class Secondary_window(Screen):
-	headgrid = ObjectProperty()
-	innerBOX = ObjectProperty()
-	heading_label = ObjectProperty()
-	description_label = ObjectProperty()
-	selection_label = ObjectProperty()
-	# print(heading_label)
-	# print(description_label)
-	# print(selection_label)
+    headgrid = ObjectProperty()
+    innerBOX = ObjectProperty()
+    heading_label = ObjectProperty()
+    description_label = ObjectProperty()
+    selection_label = ObjectProperty()
+    # print(heading_label)
+    # print(description_label)
+    # print(selection_label)
 
-	def show_error(self, errormsg="Error occured"):
-		content = ErrorDialog(cancel=self.dismiss_popup)
-		self._popup = Popup(title="Error", content=content, size_hint=(0.9, 0.9))
-		self._popup.open()
+    def show_error(self, errormsg="Error occured"):
+        content = ErrorDialog(cancel=self.dismiss_popup)
+        self._popup = Popup(title="Error", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
 
-	def dismiss_popup(self):
-		self._popup.dismiss()
+    def dismiss_popup(self):
+        self._popup.dismiss()
 
-	def show_load(self):
-		content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-		self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
-		self._popup.open()
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
 
-	def show_save(self):
-		content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
-		self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
-		self._popup.open()
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
 
-	def load(self, path, filename, auto_close=False):
-		if not filename:
-			print("please select a valid file")
-			self.show_error(errormsg="file not found")
-			return 0
-		file_path = os.path.join(path, filename[0])
-		print(file_path)
-		if self.selection_label.text == "No files loaded":
-			self.selection_label.text = file_path
-		else:
-			self.selection_label.text = self.selection_label.text + "\n" + file_path
-		if auto_close:
-			self.dismiss_popup()
+    def load(self, path, filename, auto_close=False):
+        if not filename:
+            print("please select a valid file")
+            self.show_error(errormsg="file not found")
+            return 0
+        file_path = os.path.join(path, filename[0])
+        print(file_path)
+        if self.selection_label.text == "No files loaded":
+            self.selection_label.text = file_path
+        else:
+            self.selection_label.text = self.selection_label.text + "\n" + file_path
+        if auto_close:
+            self.dismiss_popup()
 
-	def save(self, path):
-		# path=Path(path)
-		print('Folder selected for saving is ',path)
-		self.final(path)
+    def save(self, path):
+        # path=Path(path)
+        print("Folder selected for saving is ", path)
+        self.final(path)
 
-	def final(self):
-		pass
+    def final(self):
+        pass
 
-	def show_completed(self):
-		content = CompletedDialog(cancel=self.dismiss_popup)
-		self._popup = Popup(title="Completed", content=content, size_hint=(0.9, 0.9))
-		self._popup.open()
+    def show_completed(self):
+        content = CompletedDialog(cancel=self.dismiss_popup)
+        self._popup = Popup(title="Completed", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
 
 
 class CompletedDialog(FloatLayout):
-	cancel = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class LoadDialog(FloatLayout):
-	load = ObjectProperty(None)
-	cancel = ObjectProperty(None)
-
-
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class SaveDialog(FloatLayout):
-	save = ObjectProperty(None)
-	cancel = ObjectProperty(None)
-
-
+    save = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class ErrorDialog(FloatLayout):
-	cancel = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class Imagetopdf_screen(Secondary_window):
-	def __init__(self, **kw):
-		super().__init__(**kw)
-		Clock.schedule_once(self.initiation)
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.initiation)
 
-	def initiation(self, nw):
-		self.heading_label.text = "Convert Image to Pdf"
-		self.description_label.text = (
-			"Please select the images that need to be combined and converted to pdf"
-		)
+    def initiation(self, nw):
+        self.heading_label.text = "Convert Image to Pdf"
+        self.description_label.text = (
+            "Please select the images that need to be combined and converted to pdf"
+        )
 
-	def final(self, path):
-		path_list = self.selection_label.text.split("\n")
-		print(path_list)
-		path_lib_path = list(map(Path, path_list))
-		print(
-			"splitted path",
-		)
-		p = PdfEditor(path_list=path_lib_path)
-		p.imgtopdf(savepath=path)
-		self.dismiss_popup()
-		self.show_completed()
+    def final(self, path):
+        path_list = self.selection_label.text.split("\n")
+        print(path_list)
+        path_lib_path = list(map(Path, path_list))
+        print("splitted path",)
+        p = PdfEditor(path_list=path_lib_path)
+        p.imgtopdf(savepath=path)
+        self.dismiss_popup()
+        self.show_completed()
 
 
 class MergePDF_screen(Secondary_window):
-	def __init__(self, **kw):
-		super().__init__(**kw)
-		Clock.schedule_once(self.initiation)
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        Clock.schedule_once(self.initiation)
 
-	def initiation(self, nw):
-		self.heading_label.text = "Merge Pdfs"
-		self.description_label.text = "Please select the files that need to be merged"
+    def initiation(self, nw):
+        self.heading_label.text = "Merge Pdfs"
+        self.description_label.text = "Please select the files that need to be merged"
 
-	def final(self, path):
-		path_list = self.selection_label.text.split("\n")
-		print(path_list)
-		path_lib_path = list(map(Path, path_list))
-		print(
-			"splitted path",
-		)
-		p = PdfEditor(path_list=path_lib_path)
-		p.pdf_merge(savepath=path)
-		self.dismiss_popup()
-		self.show_completed()
+    def final(self, path):
+        path_list = self.selection_label.text.split("\n")
+        print(path_list)
+        path_lib_path = list(map(Path, path_list))
+        print("splitted path",)
+        p = PdfEditor(path_list=path_lib_path)
+        p.pdf_merge(savepath=path)
+        self.dismiss_popup()
+        self.show_completed()
 
 
 class Extract_page_screen(Secondary_window):
-	def __init__(self, **kw):
-		super().__init__(**kw)
-		self.input1 = ObjectProperty()
-		Clock.schedule_once(self.initiation)
-		self.my_list = ""
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.input1 = ObjectProperty()
+        Clock.schedule_once(self.initiation)
+        self.my_list = ""
 
-	def initiation(self, nw):
-		self.heading_label.text = "Extract pages from pdf"
-		self.description_label.text = "Please select the pdfs"
-		self.range_input = False
-		label_pg_start = Label(
-			text="Enter the page no.s to extract seperated by commas below for range extract enter the first page only"
-		)
-		label_pg_start.text_size = label_pg_start.size
-		self.headgrid.add_widget(label_pg_start)
-		label_pg_end = Label(
-			text="for a range extract enter the final page no. Leave blank if you are not doing a range extract"
-		)
-		label_pg_end.text_size = label_pg_end.size
-		self.headgrid.add_widget(label_pg_end)
-		self.start_page = TextInput(hint_text="start", multiline=False)
-		self.start_page.bind(text=self.check_button)
-		final_page = TextInput(hint_text="end", multiline=False)
-		final_page.bind(text=self.check_button)
-		self.headgrid.add_widget(self.start_page)
-		self.headgrid.add_widget(final_page)
-		self.ids.save_but.disabled = True
+    def initiation(self, nw):
+        self.heading_label.text = "Extract pages from pdf"
+        self.description_label.text = "Please select the pdfs"
+        self.range_input = False
+        label_pg_start = Label(
+            text="Enter the page no.s to extract seperated by commas below for range extract enter the first page only"
+        )
+        label_pg_start.text_size = label_pg_start.size
+        self.headgrid.add_widget(label_pg_start)
+        label_pg_end = Label(
+            text="for a range extract enter the final page no. Leave blank if you are not doing a range extract"
+        )
+        label_pg_end.text_size = label_pg_end.size
+        self.headgrid.add_widget(label_pg_end)
+        self.start_page = TextInput(hint_text="start", multiline=False)
+        self.start_page.bind(text=self.check_button)
+        final_page = TextInput(hint_text="end", multiline=False)
+        final_page.bind(text=self.check_button)
+        self.headgrid.add_widget(self.start_page)
+        self.headgrid.add_widget(final_page)
+        self.ids.save_but.disabled = True
 
-	def check_button(self, *args):
-		if args[0] == self.start_page:
-			self.first_page = args[1]
-			self.ids.save_but.disabled = False
-		else:
-			self.final_page = args[1]
+    def check_button(self, *args):
+        if args[0] == self.start_page:
+            self.first_page = args[1]
+            self.ids.save_but.disabled = False
+        else:
+            self.final_page = args[1]
 
-	def final(self, path):
-		print("started final class")
-		if self.first_page:
-			try:
-				if self.final_page:
-					self.my_list = [int(self.first_page) - 1, int(self.final_page) - 1]
-					print(self.my_list)
-					print(type(self.my_list[0]))
-					self.range_input = True
-			except Exception as e:
-				print(e)
-				print(
-					"Doing single page extract - if it is an error please check the try and catch statement"
-				)
-				page_list = self.first_page.split(",")
-				print(page_list)
-				# self.my_list = list(map(int, page_list))
-				self.my_list = [int(el) - 1 for el in page_list]
-				print(type(self.my_list))
-				self.range_input = False
+    def final(self, path):
+        print("started final class")
+        if self.first_page:
+            try:
+                if self.final_page:
+                    self.my_list = [int(self.first_page) - 1, int(self.final_page) - 1]
+                    print(self.my_list)
+                    print(type(self.my_list[0]))
+                    self.range_input = True
+            except Exception as e:
+                print(e)
+                print(
+                    "Doing single page extract - if it is an error please check the try and catch statement"
+                )
+                page_list = self.first_page.split(",")
+                print(page_list)
+                # self.my_list = list(map(int, page_list))
+                self.my_list = [int(el) - 1 for el in page_list]
+                print(type(self.my_list))
+                self.range_input = False
 
-		else:
-			print("Please enter the start page no. to extract")
+        else:
+            print("Please enter the start page no. to extract")
 
-		print("starting page extraction")
-		print(self.my_list)
-		print(self.range_input)
-		path_list = self.selection_label.text.split("\n")
-		print(path_list)
-		path_lib_path = list(map(Path, path_list))
-		print(
-			"splitted path",
-		)
-		p = PdfEditor(path_list=path_lib_path)
-		p.extract_page(page_nos=self.my_list, range=self.range_input, savepath=path)
-		self.dismiss_popup()
-		self.show_completed()
+        print("starting page extraction")
+        print(self.my_list)
+        print(self.range_input)
+        path_list = self.selection_label.text.split("\n")
+        print(path_list)
+        path_lib_path = list(map(Path, path_list))
+        print("splitted path",)
+        p = PdfEditor(path_list=path_lib_path)
+        p.extract_page(page_nos=self.my_list, range=self.range_input, savepath=path)
+        self.dismiss_popup()
+        self.show_completed()
 
 
 class PDFApp(App):
-	file_path = StringProperty('')
-	if platform == "android":
-		from android.storage import primary_external_storage_path
-		primary_ext_storage = primary_external_storage_path()
-		file_path=primary_ext_storage
-	elif platform== "linux":
-		file_path= str(Path.home())
-		print('File explorer file path=',file_path)
-	elif platform=="win":
-		file_path=Path.home().joinpath('temp_pdf_editor')
-		file_path.mkdir(exist_ok=True)
-		file_path = str(file_path)
-		# file_path = str(Path.home())
-		
-		print('File explorer file path=',file_path)
-	def build(self):
-		print(self.file_path,'This is the current path')
-		return Screen_manager()
+    file_path = StringProperty("")
+    if platform == "android":
+        from android.storage import primary_external_storage_path
+
+        primary_ext_storage = primary_external_storage_path()
+        file_path = primary_ext_storage
+    elif platform == "linux":
+        file_path = str(Path.home())
+        print("File explorer file path=", file_path)
+    elif platform == "win":
+        file_path = Path.home().joinpath("temp_pdf_editor")
+        file_path.mkdir(exist_ok=True)
+        file_path = str(file_path)
+        # file_path = str(Path.home())
+
+        print("File explorer file path=", file_path)
+
+    def build(self):
+        print(self.file_path, "This is the current path")
+        return Screen_manager()
 
 
 if __name__ == "__main__":
-	PDFApp().run()
+    PDFApp().run()
+
--- pdf_E.py
+++ pdf_E.py
@@ -5,91 +5,93 @@ from PIL import Image
 
 
 class PdfEditor:
-    def __init__(self,path_list='') -> None:
+    def __init__(self, path_list="") -> None:
         if len(path_list) == 0:
-            print('Sorry files do not exist please try again')
+            print("Sorry files do not exist please try again")
             return None
         else:
-            print('loading files')
+            print("loading files")
             for f in path_list:
                 # print(type(f))
-                if f.exists() and f.glob('*.pdf'):
-                    print('.')
-                    self.FileType='pdf'
-                elif f.exists() and (f.glob('*.jpg') or f.glob('*.png') or f.glob('*.jpeg')):
-                    print('*')
-                    self.FileType='pic'
+                if f.exists() and f.glob("*.pdf"):
+                    print(".")
+                    self.FileType = "pdf"
+                elif f.exists() and (
+                    f.glob("*.jpg") or f.glob("*.png") or f.glob("*.jpeg")
+                ):
+                    print("*")
+                    self.FileType = "pic"
                 else:
-                    print('Invalid file type supplied')
+                    print("Invalid file type supplied")
                     return 0
-        self.path_list=path_list
+        self.path_list = path_list
         print(str(self.path_list))
-        print('Files loaded successfully')
+        print("Files loaded successfully")
 
-                
-    def write_pdf(self,finalpdf_obj, file_name='sample.pdf'):
+    def write_pdf(self, finalpdf_obj, file_name="sample.pdf"):
         with Path(file_name).open(mode="wb") as output_file:
             finalpdf_obj.write(output_file)
-    def pdf_merge(self,savepath=''):
-        
+
+    def pdf_merge(self, savepath=""):
+
         pdf_merger = PdfFileMerger()
         for f in self.path_list:
             pdf_merger.append(str(f))
-        savepath=savepath+'\\'+str(f.name)
-        self.write_pdf(finalpdf_obj=pdf_merger,file_name=savepath)
-        print('Pdfs merged succesfully')
+        savepath = savepath + "\\" + str(f.name)
+        self.write_pdf(finalpdf_obj=pdf_merger, file_name=savepath)
+        print("Pdfs merged succesfully")
         print(savepath)
         return True
         # with Path("full_pdf.pdf").open(mode="wb") as output_file:
         #     print('all the pdf files merged')
         #     pdf_merger.write(output_file)
 
-    def extract_page(self,page_nos=[1],range=1,savepath='',save=True):
+    def extract_page(self, page_nos=[1], range=1, savepath=""):
         pdf_writer = PdfFileWriter()
-        count=0
+        count = 0
         for f in self.path_list:
             input_pdf = PdfFileReader(str(f))
             if range:
-                print('page 0 and page 1 are',page_nos[0],page_nos[1])
-                extracted_page=input_pdf.pages[page_nos[0]:page_nos[1]]
+                print("page 0 and page 1 are", page_nos[0], page_nos[1])
+                extracted_page = input_pdf.pages[page_nos[0] : page_nos[1]]
                 for xtract in extracted_page:
                     pdf_writer.addPage(xtract)
             else:
                 for pg in page_nos:
-                    print(f'extracted {count+1} no. of pages')
+                    print(f"extracted {count+1} no. of pages")
                     extracted_page = input_pdf.getPage(pg)
                     pdf_writer.addPage(extracted_page)
-        savepath=savepath+'\\'+str(f.name)
-        if save:
-            self.write_pdf(finalpdf_obj=pdf_writer,file_name=savepath+str(count)+'.pdf')
-            print(savepath+str(count)+'.pdf')
-        else:
-            return pdf_writer
-        count=count+1
+        final_path = Path(savepath, f"{str(f.name)}{str(count)}_backup.pdf")
+        self.write_pdf(finalpdf_obj=pdf_writer, file_name=final_path)
+        # savepath=savepath+'\\'+str(f.name)
+        # self.write_pdf(finalpdf_obj=pdf_writer,file_name=savepath+str(count)+'.pdf')
+        # print(savepath+str(count)+'.pdf')
+        print(final_path)
+        count = count + 1
 
-
-    def imgtopdf(self,savepath=''):
-        image_list=[]
+    def imgtopdf(self, savepath=""):
+        image_list = []
         for f in self.path_list:
             inputFile = Image.open(str(f))
-            ImageB=inputFile.convert('RGB')
+            ImageB = inputFile.convert("RGB")
             image_list.append(ImageB)
-        first_pg=image_list[0]
+        first_pg = image_list[0]
         image_list.pop(0)
-        first_pg.save(savepath+'\converted_image.pdf',save_all=True,append_images=image_list)
-        print(savepath+'\converted_image.pdf')
+        final_path = Path(savepath, "converted_image_backup.pdf")
+        first_pg.save(str(final_path), save_all=True, append_images=image_list)
+        # first_pg.save(savepath+'\converted_image.pdf',save_all=True,append_images=image_list)
+        # print(savepath+'\converted_image.pdf')
+        print(final_path)
         return True
 
-    def extract_text(self,page_nos=[1],pg_range=1,savepath=''):
+    def extract_text(self, page_nos=[1], pg_range=1, savepath=""):
 
-        writer=self.extract_page(page_nos=page_nos,range=pg_range,savepath='',save=False)
-        print('no.of pages are ',type(writer.getNumPages()))
+        writer = self.extract_page(
+            page_nos=page_nos, range=pg_range, savepath="", save=False
+        )
+        print("no.of pages are ", type(writer.getNumPages()))
         for pg in [range(writer.getNumPages())]:
             print(writer.getPage(pg))
-            
-
-            
-        
 
 
 if __name__ == "__main__":
@@ -103,26 +105,26 @@ if __name__ == "__main__":
     p.imgtopdf()
     """
 
-
     # Block for extracting pages
     getfile = Path(__file__).parents[0]
-    path_list=list(getfile.glob('pdf_files/*.pdf'))
-    p= PdfEditor(path_list=path_list)
-    range=input('If the pages needed to be extracted from a range please input 1 and enter to confirm else just press enter\n')
-    print('\nEnter the pages that needed to be extracted once over type\"stop\"\nFor range input just type first and last page number then then \"stop\"\n')
+    path_list = list(getfile.glob("pdf_files/*.pdf"))
+    p = PdfEditor(path_list=path_list)
+    range = input(
+        "If the pages needed to be extracted from a range please input 1 and enter to confirm else just press enter\n"
+    )
+    print(
+        '\nEnter the pages that needed to be extracted once over type"stop"\nFor range input just type first and last page number then then "stop"\n'
+    )
     try:
         my_list = []
         while True:
-            my_list.append(int(input()))     
+            my_list.append(int(input()))
     # if the input is not-integer, just print the list
     except:
         print(my_list)
-        # p.extract_page(page_nos=my_list,range=range)
-        p.extract_text(page_nos=my_list,pg_range=range,savepath='')
+        p.extract_page(page_nos=my_list, range=range)
 
     # Block for extracting page finishes
 
-   
     # p.pdf_merge()
 
-    
